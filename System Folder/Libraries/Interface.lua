452e734b663dd0c1b2278dd8385c4e47541e0277
ocawesome101 <ocawesome101@gmail.com>
2021-04-07 21:33:52 -0400

    a few things

--- System Folder/Libraries/Interface.lua
+++ System Folder/Libraries/Interface.lua
@@ -4,15 +4,32 @@ _G.finder = {}
 
 local _window = {}
 
-function _window:resize()
+function _window:resize(w, h)
+  self.elements:resize(w, h)
+end
+
+function _window:key(key, char)
+  self.elements:key(key, char)
+end
+
+function _window:click(x, y)
+  self.elements:click(x, y - 1)
 end
 
 function _window:close()
+  if self.elements.close then
+    self.elements:close()
+  end
 end
 
 setmetatable(_window, {__index = system._canvas})
 
-function finder.makeWindow()
+finder.windows = {}
+
+function finder.makeWindow(elements)
+  return setmetatable({
+    elements = elements,
+  }, {__index = _window})
 end
 
 function finder.start()
